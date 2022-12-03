593c45aa23e5c5f77d7ce2c3fcf847bbff4196b7
Ryan Horn <rj.horn@gmail.com>
2022-12-03 15:10:44 -0700

    commit tree e3f75ac3ba3447507735e4e4e84d751bad428cc9

---
+++ index.js
@@ -0,0 +1,68 @@
+// const chai = 'chai'
+// const spies = 'chai-spies'
+
+// function receivesAFunction(spy){
+//    // const spy = chai.spy();
+
+//     receivesAFunction(spy);
+
+
+//     return returnsANamedFunction()
+// }
+
+
+// const chai = require("chai");
+// const spies = require("chai-spies");
+// chai.use(spies);
+
+// describe("index", () => {
+//   describe("receivesAFunction(callback)", () => {
+//     it("receives a function and calls it", () => {
+//       const spy = chai.spy();
+
+//       receivesAFunction(spy);
+
+//       expect(spy).to.have.been.called();
+//     });
+//   });
+
+//   describe("returnsANamedFunction()", () => {
+//     var fn;
+
+//     before(() => {
+//       fn = returnsANamedFunction();
+//     });
+
+//     it("returns a function", () => {
+//       expect(fn).to.be.a("function");
+//     });
+
+//     it("returns a named function", () => {
+//       expect(fn.name).not.to.eql("");
+//     });
+//   });
+
+//   describe("returnsAnAnonymousFunction()", () => {
+//     var fn;
+
+//     before(() => {
+//       fn = returnsAnAnonymousFunction();
+//     });
+
+//     it("returns a function", () => {
+//       expect(fn).to.be.a("function");
+//     });
+
+//     it("returns an anonymous function", () => {
+//       expect(fn.name).to.eql("");
+//     });
+//   });
+// });
+
+
+
+// new attempt at code
+
+function receivesAFunction(Spy){
+    return "something goes here"
+}
